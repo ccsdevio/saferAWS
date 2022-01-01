28c5bac929d2699fd01635fd13d12df095c85917
manach <chris.scott22@gmail.com>
2021-12-31 22:45:14 -0500

    one more similar fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 resource "aws_api_gateway_rest_api" "test_api" {
-  name = "test1"
+  name = "test_api"
 }
 
 resource "aws_api_gateway_resource" "test1resource1" {
-  rest_api_id = aws_api_gateway_rest_api.test1.id
-  parent_id   = aws_api_gateway_rest_api.test1.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  parent_id   = aws_api_gateway_rest_api.test_api.root_resource_id
   path_part   = "test1resource1"
 }
