8d541a6e873c239e4a4a628adefff0d32a76255c
manach <chris.scott22@gmail.com>
2021-12-31 22:43:20 -0500

    change cc_api to test1 in resource declaration

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "test_api" {
 }
 
 resource "aws_api_gateway_resource" "test1resource1" {
-  rest_api_id = aws_api_gateway_rest_api.cc_api.id
-  parent_id   = aws_api_gateway_rest_api.cc_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.test1.id
+  parent_id   = aws_api_gateway_rest_api.test1.root_resource_id
   path_part   = "test1resource1"
 }
